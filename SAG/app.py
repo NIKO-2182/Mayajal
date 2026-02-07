240d76073fb7102a50332956fa1c81410aad57db
Niko <niharkore12@gmail.com>
2026-02-08 02:05:30 +0530

    fix: Updated app.py fixed async error.

--- SAG/app.py
+++ SAG/app.py
@@ -35,7 +35,7 @@ def run_async(func):
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         try:
-            return func(*args, **kwargs)
+            return loop.run_until_complete(func(*args, **kwargs))
         finally:
             loop.close()
     return wrapper
